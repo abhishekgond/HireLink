ffb7c0fd93aa9cc2f28704ce7a1338ed37ec5d09
Abhishek kumar <abhishekkumargond592@gmail.com>
2024-12-11 01:48:10 +0530

    In This  I am Fixing Some Issus Regarding dataBase Connections

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register } from "../controllers/usercontroller";
+import { register } from "../controllers/usercontroller.js";
 const router = express.Router();
 router.post("/register", register);
 export default router;
